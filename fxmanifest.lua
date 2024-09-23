6d95a2c0b225f6e7d12d0eafb7c88409fbd216a9
yaawm <68302393+yaawm@users.noreply.github.com>
2024-09-23 15:36:47 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "adamant"
+game "rdr3"
+
+author 'soda'
+description 'Valentine Upper Office'
+version '1.0.0'
+
+this_is_a_map "yes"
+
+files {'soda_sheriff_int.xml'}
+
+data_file "TIMECYCLEMOD_FILE" "soda_sheriff_int.xml"
+
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
